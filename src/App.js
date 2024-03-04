3b08ccb1e85015f306f329d46efb055513a72c6f
DumitriuAndreii <andrei24ian@gmail.com>
2024-03-04 23:58:23 +0200

    add edit functionality and design

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import React, { useState } from 'react';
+import TodoList from './components/TodoList'; // Ensure this component is created
 import './App.css';
-import TodoList from './components/TodoList';
-
 
 function App() {
-  const initialList = [
+  const [todos, setTodos] = useState([
     { id: 1, text: 'Make something great today!', completed: false },
     { id: 2, text: 'Dream Big', completed: false },
-    { id: 3, text: 'Do not stop!', completed: false }
-  ];
+    { id: 3, text: 'Improve this app! :)', completed: false }
+  ]);
   const [task, setTask] = useState('');
-  const [todos, setTodos] = useState(initialList);
-
 
   const addTask = () => {
-    if (!task) return; // Don't add empty tasks
+    if (!task) return; // Prevent adding empty tasks
     setTodos([...todos, { id: Date.now(), text: task, completed: false }]);
-    setTask(''); // Reset input field after adding
+    setTask(''); // Clear input field after adding
   };
 
   return (
-    <div className="App">
-      <div className="max-w-xl mx-auto pt-5">
-        <input
-          type="text"
-          className="input input-bordered input-primary w-full max-w-xs"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-          placeholder="Add a new task"
-        />
-        <button className="btn btn-primary ml-2" onClick={addTask}>Add</button>
-        <TodoList todos={todos} setTodos={setTodos} />
+    <div className="flex flex-col items-center min-h-full h-max w-full pt-32 bg-[#124076] overscroll-none">
+      <div className="w-full max-w-xl p-10 bg-[#F9E897] mb-10 rounded shadow flex flex-col">
+        <h1 className="text-3xl font-bold mb-4">Today's Todo List</h1>
+        <div className="mb-4 flex justify-between">
+          <input
+            type="text"
+            className="w-full  mr-4 py-2 px-4 border border-slate-900 rounded-xl"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            placeholder="Add a new task"
+            autoFocus
+          />
+          <button className="btn btn-primary border border-stone-800 py-2 px-4 rounded-lg bg-[#A4CE95]" onClick={addTask}>Add</button>
+        </div>
+        <div className="overflow-auto">
+          <TodoList todos={todos} setTodos={setTodos} />
+        </div>
       </div>
     </div>
   );
